a122587dca99bdf4df3fbbcb468cd3399b7283f5
Jim Carr <hidden-email>
2019-01-21 14:07:26 -0500

    Lite version of "C" project -> simplified Doxyfile.

---
+++ c-lite/docs/html/navtreeindex0.js
@@ -0,0 +1,13 @@
+var NAVTREEINDEX0 =
+{
+"dir_68267d1309a1af8e8297ef4c3efbcdba.html":[0,0,0],
+"files.html":[0,0],
+"globals.html":[0,1,0],
+"globals_func.html":[0,1,1],
+"index.html":[],
+"main_8c.html":[0,0,0,0],
+"main_8c.html#a15ee4fb04f86ee675899a77ddade5a1b":[0,0,0,0,2],
+"main_8c.html#a443a6e2af09b3f4dc9721f0f2cd8498c":[0,0,0,0,1],
+"main_8c.html#acdef7a1fd863a6d3770c1268cb06add3":[0,0,0,0,0],
+"pages.html":[]
+};
